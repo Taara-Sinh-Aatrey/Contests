fd9a924aa44e695caf9c3bc3d8b93b91efa282f9
Taara Sinh Aatrey <taarasinh1@gmail.com>
2021-01-30 22:05:05 +0530

    ABC190

---
+++ Atcoder/ABC190/E.cpp
@@ -0,0 +1,217 @@
+/**
+ *    author:  Taara Sinh Aatrey
+ *    created: 30.01.2021 20:03:43
+**/
+// #undef _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+string to_string(const string& s) { return '"' + s + '"'; }
+
+string to_string(const char* s) { return to_string((string) s); }
+
+string to_string(const char& ch) { return "'" + string(1, ch) + "'"; }
+
+string to_string(bool b) { return (b ? "true" : "false"); }
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifndef ONLINE_JUDGE
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+#define int int64_t
+#define ar array<int, 2>
+#define pii pair<int, int>
+#define pb push_back
+#define vt vector
+#define For(i, n) for (int i = 0; i < n; i++)
+#define Rev(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOR(i, n) for (int i = 1; i <= n; i++)
+#define REV(i, n) for (int i = n; i >= 1; i--)
+#define Rep(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define msb(x) (int) (31 - __builtin_clz(x))
+
+template <typename T>
+using mxpq = priority_queue<T>;
+template <typename T>
+using mnpq = priority_queue<T, vt<T>, greater<T>>;
+
+template <typename T1, typename T2>
+istream& operator>>(istream& in, pair<T1, T2>& a) {
+    in >> a.first >> a.second;
+    return in;
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, pair<T1, T2>& a) {
+    out << a.first << " " << a.second;
+    return out;
+}
+template <class A, size_t S>
+istream& operator>>(istream& in, array<A, S>& a) {
+    for (A& x : a) in >> x;
+    return in;
+}
+template <class A, size_t S>
+ostream& operator<<(ostream& out, array<A, S>& a) {
+    bool f = false;
+    for (A& x : a) {
+        if (f) out << " ";
+        out << x;
+        f = true;
+    }
+    return out;
+}
+template <typename T>
+istream& operator>>(istream& in, vt<T>& a) {
+    for (T& x : a) in >> x;
+    return in;
+}
+template <typename T>
+ostream& operator<<(ostream& out, vt<T>& a) {
+    bool f = false;
+    for (T& x : a) {
+        if (f) out << " ";
+        out << x;
+        f = true;
+    }
+    out << '\n';
+    return out;
+}
+void out(bool ok, bool cap = true) {
+    if (cap) cout << (ok ? "YES" : "NO") << '\n';
+    else cout << (ok ? "Yes" : "No") << '\n';
+}
+template <typename T, typename T1>
+T amax(T& a, T1 b) {
+    if (b > a) a = b;
+    return a;
+}
+template <typename T, typename T1>
+T amin(T& a, T1 b) {
+    if (b < a) a = b;
+    return a;
+}
+
+const int mod = 1e9 + 7;
+const int INF = 1e18L + 5;
+const int N = 1e5 + 5;
+
+// x | (x + 1) sets lowest unset bit of x
+// x & (x - 1) unsets lowest set bit of x
+
+vt<int> g[N], d[N];
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < m; i++) {
+    	int u, v;
+    	cin >> u >> v;
+    	u--, v--;
+    	g[u].pb(v);
+    	g[v].pb(u);
+    }
+    int k;
+    cin >> k;
+    vt<int> c(k);
+    For(i, k) {
+    	cin >> c[i];
+    	c[i]--;
+    }
+    for(auto& src: c) {
+    	vt<bool> vis(n, 0);
+    	vis[src] = 1;
+    	vt<int> qu(1, src);
+    	d[src] = vt<int>(n, INF);
+    	d[src][src] = 0;
+    	for(int x = 0; x < sz(qu); x++) {
+    		int u = qu[x];
+    		for(int& v: g[u]) {
+    			if(!vis[v]) {
+    				vis[v] = 1;
+    				d[src][v] = d[src][u] + 1;
+    				qu.pb(v);
+    			}
+    		}
+    	}
+    }
+    vt<vt<int>> dp(1 << k, vt<int>(k, INF));
+    For(i, k) {
+    	dp[1 << i][i] = 0;
+    }
+    for(int mask = 1; mask < (1 << k); mask++) {
+    	for(int last = 0; last < k; last++) {
+    		if(!(mask >> last & 1)) continue;
+    		for(int i = 0; i < k; i++) {
+	    		if(mask >> i & 1) continue;
+	    		amin(dp[mask | (1 << i)][i], dp[mask][last] + d[c[last]][c[i]]);
+	    	}
+    	}
+    }
+    int ans = *min_element(all(dp.back())) + 1;
+    if(ans >= INF) ans = -1;
+    cout << ans;
+}
+
+signed main()
+{
+#ifdef LOCAL
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    FOR(tt, t) solve();
+    return 0;
+}
