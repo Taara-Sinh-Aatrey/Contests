bca37cab4880b22aebce0fe243f545d993d8bd3f
Taara-Sinh-Aatrey <taarasinh1@gmail.com>
2021-02-04 13:58:13 +0530

    Updated Contest.py

--- CF/contest.py
+++ CF/contest.py
@@ -2,12 +2,12 @@ import json
 import urllib.request
 from bs4 import BeautifulSoup
 import os
+import subprocess
 
-url = input()
+url = input("Enter Contest Id : ")
 url = "https://codeforces.com/contest/" + url
-if( url.find('https://codeforces.com/contest') == -1):
-	with open("solution.cpp__tests", "w") as outfile:
-		outfile.write('Please open a contest page')
+if(url.find('https://codeforces.com/contest') == -1):
+	print("Invalid Url")
 	exit()
 else:
 	page = urllib.request.urlopen(url, timeout=20)
@@ -38,67 +38,44 @@ path = os.path.join(parent_dir,path)
 if not os.path.exists(path):
 	os.mkdir(path)
 
+x = url + '/submit'
+subprocess.run(["firefox", x], stdout=subprocess.DEVNULL)
+x = url + '/standings/friends/true'
+subprocess.run(["firefox", x], stdout=subprocess.DEVNULL)
+
+
 for problems in problem_tags:
 	problem_path = os.path.join(path,problems)
 	print(problem_path)
 	with open(problem_path+".cpp", "a") as sec:
 		pass
 	problem_url = " " + url + "/problem/" + problems + " \"";
-	os.system('python automate.py' + problem_url + problem_path + "\"") 
+	os.system('python automate.py' + problem_url + problem_path + "\"")
+	x = url + '/problem/' + problems
+	subprocess.run(["firefox", x], stdout=subprocess.DEVNULL)
 
-os.system('subl ' + path)
-# os.system('google-chrome ' + url + '/standings/friends/true' + " & disown")
+subprocess.run(["subl", path], stdout=subprocess.DEVNULL)
 
 
-# import json
-# import urllib.request
-# from bs4 import BeautifulSoup
-# from pywinauto import Application
-# import os
+# for firefox users
+# # for linux
+# os.system('firefox ' + url + '/submit & disown')
+# os.system('firefox ' + url + '/standings/friends/true & disown')
+# # for windows : you don't need "& disown"
+# # os.system('firefox ' + url + '/submit')
+# # os.system('firefox ' + url + '/standings/friends/true')
 
-# app = Application(backend='uia')
-# app.connect(title_re=".*Chrome.*")
-# dlg = app.top_window()
-# url = dlg.child_window(title="Address and search bar", control_type="Edit").get_value()
-
-# url = "https://" + url
-# if( url.find('https://codeforces.com/contest') == -1):
-# 	with open("solution.cpp__tests", "w") as outfile:
-# 		outfile.write('Please open a contest page')
-# 	exit()
-# else:
-# 	page = urllib.request.urlopen(url)
-# soup = BeautifulSoup(page, features = "html.parser")
-
-# data = []
-
-# table = soup.find('table', attrs={'class':'problems'})
-
-# rows = table.find_all('tr')
-# for row in rows:
-# 	cols = row.find_all('td')
-# 	cols = [ele.text.strip() for ele in cols]
-# 	data.append([ele for ele in cols if ele])
-
-# problem_tags = []
-# for i in range(len(data)):
-# 	if(len(data[i])>0):
-# 		problem_tags.append(data[i][0])
-
-# print(problem_tags)
-
-# contest_id = url[-4] + url[-3] + url[-2] + url[-1]
-# path = contest_id
-# parent_dir = os.getcwd()
-
-# path = os.path.join(parent_dir,path)
-# if not os.path.exists(path):
-# 	os.mkdir(path)
 
 # for problems in problem_tags:
 # 	problem_path = os.path.join(path,problems)
 # 	print(problem_path)
-# 	with open(problem_path+".cpp", "w") as sec:
+# 	with open(problem_path+".cpp", "a") as sec:
 # 		pass
 # 	problem_url = " " + url + "/problem/" + problems + " \"";
-# 	os.system('python automate.py' + problem_url + problem_path + "\"") 
+# 	os.system('python automate.py' + problem_url + problem_path + "\"")
+# 	# for linux
+# 	os.system('firefox ' + url + '/problem/' + problems + ' & disown')
+# 	# for windows : you don't need "& disown"
+# 	# os.system('firefox ' + url + '/problem/' + problems)
+
+# os.system('subl ' + path)
