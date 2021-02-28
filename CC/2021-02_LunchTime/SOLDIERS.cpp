4f4c68e2f396c92c321c868a207fe39f8d23f22a
Taara-Sinh-Aatrey <taarasinh1@gmail.com>
2021-02-28 17:01:19 +0530

    Feb LunchTime 2021

---
+++ CC/2021-02_LunchTime/SOLDIERS.cpp
@@ -0,0 +1,472 @@
+/**
+ *    author:  Taara Sinh Aatrey
+ *    created: 27.02.2021 22:06:44
+**/
+
+// #undef _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+string to_string(const string& s) { return '"' + s + '"'; }
+
+string to_string(const char* s) { return to_string((string) s); }
+
+string to_string(const char& ch) { return "'" + string(1, ch) + "'"; }
+
+string to_string(bool b) { return (b ? "true" : "false"); }
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+void debug_out() { cout << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << " " << to_string(H);
+    debug_out(T...);
+}
+#ifndef ONLINE_JUDGE
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+#define int int64_t
+#define ar array<int, 2>
+#define pii pair<int, int>
+#define pb push_back
+#define vt vector
+#define For(i, n) for (int i = 0; i < n; i++)
+#define Rev(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOR(i, n) for (int i = 1; i <= n; i++)
+#define REV(i, n) for (int i = n; i >= 1; i--)
+#define Rep(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define msb(x) (int) (31 - __builtin_clz(x))
+template <typename T>
+using mxpq = priority_queue<T>;
+template <typename T>
+using mnpq = priority_queue<T, vt<T>, greater<T>>;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int a, int b) { return uniform_int_distribution<int>(a, b)(rng); }
+
+template <typename T1, typename T2>
+istream& operator>>(istream& in, pair<T1, T2>& a) {
+    in >> a.first >> a.second;
+    return in;
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, pair<T1, T2>& a) {
+    out << a.first << " " << a.second;
+    return out;
+}
+template <class A, size_t S>
+istream& operator>>(istream& in, array<A, S>& a) {
+    for (A& x : a) in >> x;
+    return in;
+}
+template <class A, size_t S>
+ostream& operator<<(ostream& out, array<A, S>& a) {
+    bool f = false;
+    for (A& x : a) {
+        if (f) out << " ";
+        out << x;
+        f = true;
+    }
+    return out;
+}
+template <typename T>
+istream& operator>>(istream& in, vt<T>& a) {
+    for (T& x : a) in >> x;
+    return in;
+}
+template <typename T>
+ostream& operator<<(ostream& out, vt<T>& a) {
+    bool f = false;
+    for (T& x : a) {
+        if (f) out << " ";
+        out << x;
+        f = true;
+    }
+    return out;
+}
+void out(bool ok, bool cap = true) {
+    if (cap) cout << (ok ? "YES" : "NO") << '\n';
+    else cout << (ok ? "Yes" : "No") << '\n';
+}
+template <typename T, typename T1>
+T amax(T& a, T1 b) {
+    if (b > a) a = b;
+    return a;
+}
+template <typename T, typename T1>
+T amin(T& a, T1 b) {
+    if (b < a) a = b;
+    return a;
+}
+
+const int mod = 1e9 + 7;
+const int INF = 1e18L + 5;
+const int N = 2e5 + 5;
+
+// segtree for max (min and maybe sum should work the same) with assignment modification
+// for increment modification, code should be modified accordingly
+class segtree {
+ public:
+  struct node {
+    // don't forget to set default value (used for leaves)
+    // not necessarily neutral element!
+    int mx = -INF;
+    int lazy = -INF;
+ 
+    void apply(int l, int r, int v) {
+      lazy = max(lazy, v);
+      mx = max(mx, v);
+    }
+  };
+ 
+  node unite(const node &a, const node &b) const {
+    node res;
+    res.mx = max(a.mx, b.mx);
+    return res;
+  }
+ 
+  inline void push(int x, int l, int r) {
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    if (tree[x].lazy != 0) {
+      tree[x + 1].apply(l, y, tree[x].lazy);
+      tree[z].apply(y + 1, r, tree[x].lazy);
+      tree[x].lazy = 0;
+    }
+  }
+ 
+  inline void pull(int x, int z) {
+    tree[x] = unite(tree[x + 1], tree[z]);
+  }
+ 
+  int n;
+  vector<node> tree;
+ 
+  void build(int x, int l, int r) {
+    if (l == r) {
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    build(x + 1, l, y);
+    build(z, y + 1, r);
+    pull(x, z);
+  }
+ 
+  template <typename M>
+  void build(int x, int l, int r, const vector<M> &v) {
+    if (l == r) {
+      tree[x].apply(l, r, v[l]);
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    build(x + 1, l, y, v);
+    build(z, y + 1, r, v);
+    pull(x, z);
+  }
+ 
+  node get(int x, int l, int r, int ll, int rr) {
+    if (ll <= l && r <= rr) {
+      return tree[x];
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    push(x, l, r);
+    node res{};
+    if (rr <= y) {
+      res = get(x + 1, l, y, ll, rr);
+    } else {
+      if (ll > y) {
+        res = get(z, y + 1, r, ll, rr);
+      } else {
+        res = unite(get(x + 1, l, y, ll, rr), get(z, y + 1, r, ll, rr));
+      }
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  template <typename... M>
+  void modify(int x, int l, int r, int ll, int rr, const M&... v) {
+    if (ll <= l && r <= rr) {
+      tree[x].apply(l, r, v...);
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    push(x, l, r);
+    if (ll <= y) {
+      modify(x + 1, l, y, ll, rr, v...);
+    }
+    if (rr > y) {
+      modify(z, y + 1, r, ll, rr, v...);
+    }
+    pull(x, z);
+  }
+ 
+  int find_first_knowingly(int x, int l, int r, const function<bool(const node&)> &f) {
+    if (l == r) {
+      return l;
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res;
+    if (f(tree[x + 1])) {
+      res = find_first_knowingly(x + 1, l, y, f);
+    } else {
+      res = find_first_knowingly(z, y + 1, r, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_first(int x, int l, int r, int ll, int rr, const function<bool(const node&)> &f) {
+    if (ll <= l && r <= rr) {
+      if (!f(tree[x])) {
+        return -1;
+      }
+      return find_first_knowingly(x, l, r, f);
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res = -1;
+    if (ll <= y) {
+      res = find_first(x + 1, l, y, ll, rr, f);
+    }
+    if (rr > y && res == -1) {
+      res = find_first(z, y + 1, r, ll, rr, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_last_knowingly(int x, int l, int r, const function<bool(const node&)> &f) {
+    if (l == r) {
+      return l;
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res;
+    if (f(tree[z])) {
+      res = find_last_knowingly(z, y + 1, r, f);
+    } else {
+      res = find_last_knowingly(x + 1, l, y, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_last(int x, int l, int r, int ll, int rr, const function<bool(const node&)> &f) {
+    if (ll <= l && r <= rr) {
+      if (!f(tree[x])) {
+        return -1;
+      }
+      return find_last_knowingly(x, l, r, f);
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res = -1;
+    if (rr > y) {
+      res = find_last(z, y + 1, r, ll, rr, f);
+    }
+    if (ll <= y && res == -1) {
+      res = find_last(x + 1, l, y, ll, rr, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  segtree(int _n) : n(_n) {
+    assert(n > 0);
+    tree.resize(2 * n - 1);
+    build(0, 0, n - 1);
+  }
+ 
+  template <typename M>
+  segtree(const vector<M> &v) {
+    n = v.size();
+    assert(n > 0);
+    tree.resize(2 * n - 1);
+    build(0, 0, n - 1, v);
+  }
+ 
+  node get(int ll, int rr) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return get(0, 0, n - 1, ll, rr);
+  }
+ 
+  node get(int p) {
+    assert(0 <= p && p <= n - 1);
+    return get(0, 0, n - 1, p, p);
+  }
+ 
+  template <typename... M>
+  void modify(int ll, int rr, const M&... v) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    modify(0, 0, n - 1, ll, rr, v...);
+  }
+ 
+  // find_first and find_last call all FALSE elements
+  // to the left (right) of the sought position exactly once
+ 
+  int find_first(int ll, int rr, const function<bool(const node&)> &f) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return find_first(0, 0, n - 1, ll, rr, f);
+  }
+ 
+  int find_last(int ll, int rr, const function<bool(const node&)> &f) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return find_last(0, 0, n - 1, ll, rr, f);
+  }
+};
+
+
+/*
+we need lmax and rmnin for every point (can be easily done using lazy seg tree)
+if two points have same pair of lmax and rmin, that means 
+they have to be considered together
+
+ex1
+
+1    2    3    4
+
+[         ]
+    [          ]
+
+point lmax rmin
+1       1    3
+2       2    3
+3       2    3
+4       2    4
+
+conclusion : we should consider (1) (23) and (4)
+Final answer : 1324
+
+ex2
+
+1    2    3    4
+
+[              ]
+    [     ]
+
+point lmax rmin
+1       1    4
+2       2    3n
+3       2    3
+4       1    4
+
+conclusion : we should consider (14) and (23)
+Final answer : 4321
+
+*/
+
+int p;
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    segtree lmax(n + 1), rmin(n + 1);    while(q--) {
+        int l, r;
+        cin >> l >> r;
+        lmax.modify(l, r, l);
+        rmin.modify(l, r, -r);
+    }
+    map<pii, vt<int>> mp;
+    vt<int> ans(n + 1, n);
+    for(int i = 1; i <= n; i++) {
+        int l = lmax.get(i).mx;
+        int r = -rmin.get(i).mx;
+        if(p == 0) {
+            if(l >= 1 && r <= n) {
+                ans[i] = i;
+            } else {
+                ans[i] = 1;
+            }
+            continue;
+        }
+        if(l >= 1 && r <= n) {
+            mp[{l, r}].push_back(i);
+        }
+    }
+    for(auto& [a, vec] : mp) {
+        int m = sz(vec);
+        for(int i = 0; i < m; i++) {
+            ans[vec[i]] = vec[m - 1 - i];
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << '\n';
+}
+
+signed main()
+{
+#ifdef LOCAL
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t >> p;
+    FOR(tt, t) solve();
+    return 0;
+}
+
+/*
+1  2  3  4
+[     ]
+      [  ]
+[        ]
+   []
+
+point lmax rmin
+1       1    3
+2       2    2
+3       3    3
+4       3    4
+*/
