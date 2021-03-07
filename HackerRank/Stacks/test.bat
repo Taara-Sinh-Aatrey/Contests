0bf798771eb350febb955b94d0f0ddb981d9fbf6
Taara-Sinh-Aatrey <taarasinh1@gmail.com>
2021-03-07 22:17:20 +0530

    deleted useless files

--- HackerRank/Stacks/test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-gen >in
-sol <in >out
-brute <in >ok
-fc out ok
-if errorlevel 1 exit
-test
--- HackerRank/Stacks/test.sh
+++
@@ -1,7 +0,0 @@
-for((i = 1; ; i++)); do
-    # echo $i
-    ./gen.out > in
-    python himanshu.py < in > out
-    ./COLGLF4.out < in > ok
-    diff -w out ok || break
-done
