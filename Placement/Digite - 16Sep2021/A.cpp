5a78e2988af1bdae5d37de3e11ee960893da4cb0
Taara-Sinh-Aatrey <taarasinh1@gmail.com>
2021-09-16 19:12:07 +0530

    Update

---
+++ Placement/Digite - 16Sep2021/A.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+bool debug;
+template <typename T, typename U> istream& operator>>(istream &is, pair<T, U> &p) { return is >> p.first >> p.second;}
+template <typename T, typename U> ostream& operator<<(ostream &os, const pair<T, U> &p) { return debug ? os << "(" << p.first << ", " << p.second << ")" : os << p.first << " " << p.second; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> istream &operator>>(istream &is, T_container &v) { for (T &x : v) is >> x; return is; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream &operator<<(ostream &os, const T_container &v) { bool f = false; if(debug) { os << "{"; for (const T &x : v) { if (f) os << ", "; os << x; f = true; } os << "}"; } else { for (const T &x : v) { if (f) os << " "; os << x; f = true; } } return os; }
+void scan() {} template <typename Head, typename ...Tail> void scan(Head &H, Tail &...T) { cin >> H; scan(T...); }
+void print() { cout << "\n"; } template <typename Head> void print(const Head &H) { cout << H << "\n"; } template <typename Head, typename ...Tail> void print(const Head &H, const Tail &...T) { cout << H << " "; print(T...); }
+void dbg_out() { cerr << "\n"; } template <typename Head, typename ...Tail> void dbg_out(const Head &H, const Tail &...T) { cerr << " " << H; dbg_out(T...); }
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", debug = true, dbg_out(__VA_ARGS__), debug = false
+#else
+#define dbg(...)
+#endif
+
+#define int int64_t
+const int inf = 1e18L + 5, mod = 1e9 + 7, N = 2e5 + 5;
+
+pair<int, int> inTheMidd(int n, int q, int x, int a[]) {
+    sort(a, a + n);
+    if(q == 2)  {
+        reverse(a, a + n);
+    }
+    int left = 0, right = 0, reached = 0;
+    for(int i = 0; i < n; i++) {
+        if(a[i] == x) {
+            reached = 1;
+        }
+        else if(reached == 0) {
+            left += (a[i] & 1);
+        }
+        else {
+            right += a[i] % 2 == 0;
+        }
+    }
+    return make_pair(left, right);
+}
+
+signed main() {
+    int arr[] = {6, 4, 3, 5, 2, 1};
+    pair<int, int> res = inTheMidd(6, 1, 3, arr);
+    cout << res.first << " " << res.second << '\n';
+    return 0;
+}
--- Placement/GamesKraft- 6Aug2021/C.cpp
+++ Placement/GamesKraft- 6Aug2021/C.cpp
@@ -8,10 +8,10 @@ ll taxiDriver(vector<ll> pickup, vector<ll> drop, vector<ll> tip) {
     vector<ll> order(n);
     iota(order.begin(), order.end(), 0);
     sort(order.begin(), order.end(), [&](ll i, ll j) {
-        if(pickup[i] == pickup[j]) {
-            return drop[i] < drop[j];
+        if(drop[i] == drop[j]) {
+            return pickup[i] < pickup[j];
         }
-        return pickup[i] < pickup[j];
+        return drop[i] < drop[j];
     });
     
     map<ll, ll> mp;
@@ -37,6 +37,8 @@ int main()
 {
     cout << taxiDriver({1, 4}, {5, 6}, {2, 5}) << endl;
     cout << taxiDriver({0, 4, 5}, {3, 5, 7}, {1, 2, 2}) << endl;
+    cout << taxiDriver({0, 2, 9, 10, 11, 12}, {5, 9, 11, 11, 14, 17}, {1, 2, 3, 2, 2, 1}) << endl;
+    cout << taxiDriver({1, 4, 7}, {5, 7, 11}, {0, 0, 0}) << endl;
     
     return 0;
 }
