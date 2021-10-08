e2a1f24cbe827845573b14f1e3d09c0abc0897c9
Taara-Sinh-Aatrey <taarasinh1@gmail.com>
2021-10-08 18:48:52 +0530

    Update

---
+++ CC/Codolympics Prelims/CDLYP21F.cpp
@@ -0,0 +1,308 @@
+#include "bits/stdc++.h"
+#include <ios>
+using namespace std;
+
+bool debug;
+template <typename T, typename U> istream& operator>>(istream &is, pair<T, U> &p) { return is >> p.first >> p.second;}
+template <typename T, typename U> ostream& operator<<(ostream &os, const pair<T, U> &p) { return debug ? os << "(" << p.first << ", " << p.second << ")" : os << p.first << " " << p.second; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> istream &operator>>(istream &is, T_container &v) { for (T &x : v) is >> x; return is; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream &operator<<(ostream &os, const T_container &v) { bool f = false; if(debug) { os << "{"; for (const T &x : v) { if (f) os << ", "; os << x; f = true; } os << "}"; } else { for (const T &x : v) { if (f) os << " "; os << x; f = true; } } return os; }
+void scan() {} template <typename Head, typename ...Tail> void scan(Head &H, Tail &...T) { cin >> H; scan(T...); }
+void print() { cout << "\n"; } template <typename Head> void print(const Head &H) { cout << H << "\n"; } template <typename Head, typename ...Tail> void print(const Head &H, const Tail &...T) { cout << H << " "; print(T...); }
+void dbg_out() { cerr << "\n"; } template <typename Head, typename ...Tail> void dbg_out(const Head &H, const Tail &...T) { cerr << " " << H; dbg_out(T...); }
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", debug = true, dbg_out(__VA_ARGS__), debug = false
+#else
+#define dbg(...)
+#endif
+
+#define int int64_t
+const int inf = 1e18L + 5, mod = 1e9 + 7, N = 2e5 + 5;
+
+class segtree {
+public:
+    struct node {
+        // don't forget to set default value (used for leaves)
+        // not necessarily neutral element!
+        int mn = inf;
+
+        void apply(int l, int r, int v) {
+            mn = v;
+        }
+    };
+
+    node unite(const node &a, const node &b) const {
+        node res;
+        res.mn = min(a.mn, b.mn);
+        return res;
+    }
+
+    inline void push(int x, int l, int r) {
+    }
+
+    inline void pull(int x, int z) {
+        tree[x] = unite(tree[x + 1], tree[z]);
+    }
+
+    int n;
+    vector<node> tree;
+
+    void build(int x, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int y = (l + r) >> 1;
+        int z = x + ((y - l + 1) << 1);
+        build(x + 1, l, y);
+        build(z, y + 1, r);
+        pull(x, z);
+    }
+
+    template <typename M>
+    void build(int x, int l, int r, const vector<M> &v) {
+        if (l == r) {
+            tree[x].apply(l, r, v[l]);
+            return;
+        }
+        int y = (l + r) >> 1;
+        int z = x + ((y - l + 1) << 1);
+        build(x + 1, l, y, v);
+        build(z, y + 1, r, v);
+        pull(x, z);
+    }
+
+    node get(int x, int l, int r, int ll, int rr) {
+        if (ll <= l && r <= rr) {
+            return tree[x];
+        }
+        int y = (l + r) >> 1;
+        int z = x + ((y - l + 1) << 1);
+        push(x, l, r);
+        node res{};
+        if (rr <= y) {
+            res = get(x + 1, l, y, ll, rr);
+        } 
+        else {
+            if (ll > y) {
+                res = get(z, y + 1, r, ll, rr);
+            } 
+            else {
+                res = unite(get(x + 1, l, y, ll, rr), get(z, y + 1, r, ll, rr));
+            }
+        }
+        pull(x, z);
+        return res;
+    }
+
+    template <typename... M>
+    void modify(int x, int l, int r, int ll, int rr, const M&... v) {
+        if (ll <= l && r <= rr) {
+            tree[x].apply(l, r, v...);
+            return;
+        }
+        int y = (l + r) >> 1;
+        int z = x + ((y - l + 1) << 1);
+        push(x, l, r);
+        if (ll <= y) {
+            modify(x + 1, l, y, ll, rr, v...);
+        }
+        if (rr > y) {
+            modify(z, y + 1, r, ll, rr, v...);
+        }
+        pull(x, z);
+    }
+
+    int find_first_knowingly(int x, int l, int r, const function<bool(const node&)> &f) {
+        if (l == r) {
+            return f(tree[x]) ? l : -1;
+        }
+        push(x, l, r);
+        int y = (l + r) >> 1;
+        int z = x + ((y - l + 1) << 1);
+        int res = -1;
+        if (f(tree[x + 1])) {
+            res = find_first_knowingly(x + 1, l, y, f);
+        } 
+        else if(f(tree[z])) {
+            res = find_first_knowingly(z, y + 1, r, f);
+        }
+        pull(x, z);
+        return res;
+    }
+
+    int find_first(int x, int l, int r, int ll, int rr, const function<bool(const node&)> &f) {
+        if (ll <= l && r <= rr) {
+            if (!f(tree[x])) {
+                return -1;
+            }
+            return find_first_knowingly(x, l, r, f);
+        }
+        push(x, l, r);
+        int y = (l + r) >> 1;
+        int z = x + ((y - l + 1) << 1);
+        int res = -1;
+        if (ll <= y) {
+            res = find_first(x + 1, l, y, ll, rr, f);
+        }
+        if (rr > y && res == -1) {
+            res = find_first(z, y + 1, r, ll, rr, f);
+        }
+        pull(x, z);
+        return res;
+    }
+
+    int find_last_knowingly(int x, int l, int r, const function<bool(const node&)> &f) {
+        if (l == r) {
+            return f(tree[x]) ? l : -1;
+        }
+        push(x, l, r);
+        int y = (l + r) >> 1;
+        int z = x + ((y - l + 1) << 1);
+        int res = -1;
+        if (f(tree[z])) {
+            res = find_last_knowingly(z, y + 1, r, f);
+        } 
+        else if(f(tree[x + 1])) {
+            res = find_last_knowingly(x + 1, l, y, f);
+        }
+        pull(x, z);
+        return res;
+    }
+
+    int find_last(int x, int l, int r, int ll, int rr, const function<bool(const node&)> &f) {
+        if (ll <= l && r <= rr) {
+            if (!f(tree[x])) {
+                return -1;
+            }
+            return find_last_knowingly(x, l, r, f);
+        }
+        push(x, l, r);
+        int y = (l + r) >> 1;
+        int z = x + ((y - l + 1) << 1);
+        int res = -1;
+        if (rr > y) {
+            res = find_last(z, y + 1, r, ll, rr, f);
+        }
+        if (ll <= y && res == -1) {
+            res = find_last(x + 1, l, y, ll, rr, f);
+        }
+        pull(x, z);
+        return res;
+    }
+
+    segtree(int _n) : n(_n) {
+        assert(n > 0);
+        tree.resize(2 * n - 1);
+        build(0, 0, n - 1);
+    }
+
+    template <typename M>
+    segtree(const vector<M> &v) {
+        n = v.size();
+        assert(n > 0);
+        tree.resize(2 * n - 1);
+        build(0, 0, n - 1, v);
+    }
+
+    node get(int ll, int rr) {
+        assert(0 <= ll && ll <= rr && rr <= n - 1);
+        return get(0, 0, n - 1, ll, rr);
+    }
+
+    node get(int p) {
+        assert(0 <= p && p <= n - 1);
+        return get(0, 0, n - 1, p, p);
+    }
+
+    template <typename... M>
+    void modify(int ll, int rr, const M&... v) {
+        assert(0 <= ll && ll <= rr && rr <= n - 1);
+        modify(0, 0, n - 1, ll, rr, v...);
+    }
+
+    // find_first and find_last call all FALSE elements
+    // to the left (right) of the sought position exactly once
+
+    int find_first(int ll, int rr, const function<bool(const node&)> &f) {
+        assert(0 <= ll && ll <= rr && rr <= n - 1);
+        return find_first(0, 0, n - 1, ll, rr, f);
+    }
+
+    int find_last(int ll, int rr, const function<bool(const node&)> &f) {
+        assert(0 <= ll && ll <= rr && rr <= n - 1);
+        return find_last(0, 0, n - 1, ll, rr, f);
+    }
+};
+
+void solve() {
+    int n, k;
+    scan(n, k);
+    vector<int> masks(n);
+    for (int i = 0; i < n; i++) {
+        int k;
+        scan(k);
+        auto& mask = masks[i];
+        for (int it = 0; it < k; it++) {
+            int x;
+            scan(x);
+            --x;
+            mask |= 1LL << x;
+        }
+    }
+    segtree st(n);
+    set<pair<int, int>> s;
+    int mask = 0;
+    map<int, int> suff;
+    suff[0] = n;
+    for (int l = n - 1; l >= 0; l--) {
+        mask ^= masks[l];
+        int req = ((1LL << k) - 1) ^ mask;
+        if (suff.count(req)) {
+            int r = suff[req] - 1;
+            st.modify(l, l, r - l + 1);
+            s.emplace(r, l);
+        }
+        suff[mask] = l;
+    }
+    int q;
+    scan(q); 
+    vector<array<int, 3>> Q(q);
+    for (int i = 0; i < q; i++) {
+        scan(Q[i][0], Q[i][1]);
+        Q[i][2] = i;
+    }
+    sort(Q.begin(), Q.end(), [&](auto& i, auto& j) {
+        if (i[1] == j[1]) {
+            return i[0] > j[0];
+        }
+        return i[1] > j[1];
+    });
+    vector<int> ans(q);
+    for (auto& p : Q) {
+        int l = p[0], r = p[1], idx = p[2];
+        --l, --r;
+        while (!s.empty() && prev(s.end())->first > r) {
+            auto& [y, x] = *prev(s.end());
+            st.modify(x, x, inf);
+            s.erase(prev(s.end()));
+        }
+        int mn = st.get(l, r).mn;
+        ans[idx] = mn < inf ? mn : -1;
+    }
+    for (auto& x : ans) print(x);
+}
+
+signed main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+
+    for (int tt = 1; tt <= t; tt++) {
+        solve();
+    }
+
+    return 0;
+}
