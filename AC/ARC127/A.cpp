2e238a7e2cf70ae35de5705fdfbe69d01ba5ae72
Taara Sinh Aatrey <taarasinhaatrey@MBA-M1.local>
2021-10-15 09:14:24 +0530

    Update

---
+++ AC/ARC127/A.cpp
@@ -0,0 +1,57 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+bool debug;
+template <typename T, typename U> istream& operator>>(istream &is, pair<T, U> &p) { return is >> p.first >> p.second;}
+template <typename T, typename U> ostream& operator<<(ostream &os, const pair<T, U> &p) { return debug ? os << "(" << p.first << ", " << p.second << ")" : os << p.first << " " << p.second; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> istream &operator>>(istream &is, T_container &v) { for (T &x : v) is >> x; return is; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream &operator<<(ostream &os, const T_container &v) { bool f = false; if(debug) { os << "{"; for (const T &x : v) { if (f) os << ", "; os << x; f = true; } os << "}"; } else { for (const T &x : v) { if (f) os << " "; os << x; f = true; } } return os; }
+void scan() {} template <typename Head, typename ...Tail> void scan(Head &H, Tail &...T) { cin >> H; scan(T...); }
+void print() { cout << "\n"; } template <typename Head> void print(const Head &H) { cout << H << "\n"; } template <typename Head, typename ...Tail> void print(const Head &H, const Tail &...T) { cout << H << " "; print(T...); }
+void dbg_out() { cerr << "\n"; } template <typename Head, typename ...Tail> void dbg_out(const Head &H, const Tail &...T) { cerr << " " << H; dbg_out(T...); }
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", debug = true, dbg_out(__VA_ARGS__), debug = false
+#else
+#define dbg(...)
+#endif
+
+#define int int64_t
+const int inf = 1e18L + 5, mod = 1e9 + 7, N = 2e5 + 5;
+
+void solve() {
+    int num;
+    scan(num);
+    string s = to_string(num);
+    int n = s.size();
+    int ans = 0;
+    int p = 1;
+    for (int i = 0; i < n; i++) {
+        p *= 10;
+    }
+    int so_far = 0;
+    for (int i = 0; i < n; i++) {
+        p /= 10;
+        int D = s[i] - '0';
+        so_far *= 10;
+        so_far += 1;
+        ans += i * p;
+        if(so_far == num / p) {
+            ans += 1 + (num % p);
+        }
+        else if(so_far < num / p) {
+            ans += p;
+        }
+    }
+    print(ans);
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    for (int tt = 1; tt <= t; tt++) {
+        solve();
+    }
+    return 0;
+}
