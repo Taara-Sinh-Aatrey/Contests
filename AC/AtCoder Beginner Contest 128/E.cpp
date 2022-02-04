d90b3021611947fe5c9ddb2494ad1f0db59f5e46
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-02-05 01:19:16 +0530

    Update

---
+++ AC/AtCoder Beginner Contest 128/E.cpp
@@ -0,0 +1,61 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+template <typename T, typename U> istream& operator>>(istream &is, pair<T, U> &p) { return is >> p.first >> p.second; }
+template <typename T, typename U> ostream& operator<<(ostream &os, const pair<T, U> &p) { return os << p.first << " " << p.second; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> istream &operator>>(istream &is, T_container &v) { for (T &x : v) is >> x; return is; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream &operator<<(ostream &os, const T_container &v) { bool f = false; for (const T &x : v) { if (f) os << " "; os << x; f = true; } return os; }
+void scan() {} template <typename Head, typename ...Tail> void scan(Head &H, Tail &...T) { cin >> H; scan(T...); }
+void print() { cout << "\n"; } template <typename Head> void print(const Head &H) { cout << H << "\n"; } template <typename Head, typename ...Tail> void print(const Head &H, const Tail &...T) { cout << H << " "; print(T...); }
+template <typename T, typename T1, typename... Tail> T amin(T& a, T1 b, Tail... c) { if(b < a) a = b; if constexpr (sizeof...(c) != 0) { amin(a, c...); } return a; } template <typename T, typename T1, typename... Tail> T amax(T& a, T1 b, Tail... c) { if(b > a) a = b; if constexpr (sizeof...(c) != 0) { amax(a, c...); } return a; }
+
+#ifdef LOCAL
+#include "debug.hpp"
+#else
+#define dbg(...)
+#endif
+
+#define int int64_t
+const int inf = 1e18L + 5, mod = 1e9 + 7, N = 2e5 + 5;
+
+void solve() {
+    int n, q;
+    scan(n, q);
+    vector<array<int, 3>> a(n);
+    for (auto &[x, s, t] : a) {
+        scan(s, t, x);
+        --t;
+    }
+    sort(a.begin(), a.end());
+    vector<int> ans(q, -1);
+    set<pair<int, int>> st;
+    for (int i = 0; i < q; i++) {
+        int start;
+        scan(start);
+        st.insert({start, i});
+    }
+    for (auto &[x, s, t] : a) {
+        int lo = s - x;
+        int hi = t - x;
+        auto it = st.lower_bound({lo, 0});
+        auto it2 = it;
+        while (it2 != st.end() && it2->first <= hi) {
+            ans[it2->second] = x;
+            it2++;
+        }
+        st.erase(it, it2);
+    }
+    for (auto &x : ans)
+        print(x);
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    for (int tt = 1; tt <= t; tt++) {
+        solve();
+    }
+    return 0;
+}
