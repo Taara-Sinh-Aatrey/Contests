f01c96a86ed6822fdf5a9f06ac8ee24177cbba46
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-08-28 22:19:21 +0530

    Solved E using KMP26

--- CF/EducationalRound134/E.cpp
+++ CF/EducationalRound134/E.cpp
@@ -1,6 +1,6 @@
 /**
  *    author:  Taara Sinh Aatrey
- *    created: 27.08.2022 21:30:21
+ *    created: 28.08.2022 21:59:18
 **/
 
 #include "bits/stdc++.h"
@@ -13,7 +13,55 @@ using namespace std;
 #define dbg(...)
 #endif
 
-#define int int64_t
+// #define int int64_t
+
+// TODO: set these consts accordinlgy
+const char MIN_CHAR = 'a';
+const int ALPHABET = 26;
+
+namespace KMP26 {
+    template <typename T>
+    void compute_link(const T &s, vector<array<int, ALPHABET>> &link, vector<int> &fail) {
+        int n = s.size();
+        // fail[i + 1] means the index where pref[index] doesn't match
+        // link[i][c] means the index where pref[index] doesn't match if we set s[i]=c
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - MIN_CHAR;
+            
+            // right now link[i][c] stores the answer for proper prefix
+            int previous = link[i][c];
+            fail[i] = previous;
+            
+            // but now we can take whole prefix for the further calculations
+            // example: ababac
+            // link[3][1] = 2
+            // but after processing i=3, we can set link[3][1] = 4
+            // so that it updates the correct value to link[5][1]
+            link[i][c] = i + 1;
+            
+            // `i` matches `previous-1`, so `link[i+1]` should be mapped to `link[previous]`
+            link[i + 1] = link[previous];
+        }
+    }
+
+    // Find all indices where `pattern` occurs within `text`
+    template<typename T>
+    vector<int> find_matches(const T &pattern, const T &text, const vector<array<int, ALPHABET>> &link) {
+        if (pattern.size() > text.size())
+            return {};
+        vector<int> matches;
+        int n = pattern.size(), m = text.size();
+        int len = 0;
+        for (int i = 0; i < m; i++) {
+            int c = text[i] - MIN_CHAR;
+            len = link[len][c];
+            // Check for a match whose last character is at index i
+            if (len == n)
+                matches.push_back(i - (n - 1));
+        }
+        return matches;
+    }
+}
 
 signed main() {
     ios::sync_with_stdio(0);
@@ -21,41 +69,26 @@ signed main() {
     string s;
     cin >> s;
     int n = s.size();
-    int len = 0;
-    vector<int> lps(n + 10, 0);
-    for (int i = 1; i < n; i++) {
-        while (len > 0 && s[i] != s[len])
-            len = lps[len - 1];
-        if (s[i] == s[len]) 
-            len++;
-        lps[i] = len;
-    }
-    int save = len;
+    const int N = 1e6, L_MAX = 10;
+    vector<array<int, ALPHABET>> link(N + L_MAX + 1, array<int, ALPHABET>());
+    vector<int> fail(N + L_MAX, 0);
+    KMP26::compute_link(s, link, fail);
+    auto link_n = link[n];
     int q;
     cin >> q;
-    map<string, int> mp;
     while (q--) {
         string t;
         cin >> t;
         int m = t.size();
-        len = save;
-        string temp;
         for (int i = n; i < n + m; i++) {
-            temp += t[i - n];
-            if (mp.count(temp)) {
-                len = mp[temp];
-            }
-            else {
-                while (len > 0 && t[i - n] != (len < n ? s[len] : t[len - n]))
-                    len = lps[len - 1];
-                if (t[i - n] == (len < n ? s[len] : t[len - n])) 
-                    len++;
-                mp[temp] = len;
-            }
-            lps[i] = len;
-            cout << lps[i] << " ";
+            int c = t[i - n] - MIN_CHAR;
+            int previous = link[i][c];
+            fail[i] = previous;
+            link[i][c] = i + 1;
+            link[i + 1] = link[previous];
+            cout << fail[i] << " \n"[i == n + m - 1];
         }
-        cout << '\n';
+        link[n] = link_n;
     }
     return 0;
 }
